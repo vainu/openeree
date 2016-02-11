1cdf89d228669fc6580f64e5fd33b8417293d6f4
renereose <rene_reose@hotmail.com>
2016-02-11 13:33:04 +0200

    styles

--- public/js/app.js
+++ public/js/app.js
@@ -17,16 +17,37 @@ var App = new(function(){
 				var data = google.visualization.arrayToDataTable(graphData);
 				console.log('DRAWING');
 				var options = {
-			        title: 'Party Members',
-			        chartArea: {width: '50%'},
-			        hAxis: {
-			          title: 'Total Members',
-			          minValue: 0
-			        },
-			        vAxis: {
-			          title: 'Party'
-			        }
-			      };
+				  hAxis: {
+				    titleTextStyle: {color: '#607d8b'}, 
+				    gridlines: { count:0}, 
+				    textStyle: { color: '#b0bec5', fontName: 'Roboto', fontSize: '12', bold: true}
+				  },
+				  vAxis: {
+				    minValue: 0, 
+				    gridlines: {color:'#37474f', count:4}, 
+				    baselineColor: 'transparent'
+				  },
+				  legend: {position: 'top', alignment: 'center', textStyle: {color:'#607d8b', fontName: 'Roboto', fontSize: '12'} },
+				  colors: ["#3f51b5","#2196f3","#03a9f4","#00bcd4","#009688","#4caf50","#8bc34a","#cddc39"],
+				  areaOpacity: 0.24,
+				  lineWidth: 1,
+				  backgroundColor: 'transparent',
+				  chartArea: {
+				    backgroundColor: "transparent",
+				    width: '100%',
+				    height: '80%'
+				  },
+				      height:200, // example height, to make the demo charts equal size
+				      width:400,
+				      pieSliceBorderColor: '#263238',
+				      pieSliceTextStyle:  {color:'#607d8b' },
+				      pieHole: 0.9,
+				      bar: {groupWidth: "40" },
+				      colorAxis: {colors: ["#3f51b5","#2196f3","#03a9f4","#00bcd4"] },
+				      backgroundColor: 'transparent',
+				      datalessRegionColor: '#37474f',
+				      displayMode: 'regions'
+				    };
 
 				var chart = new google.visualization.BarChart(document.getElementById('members_top_chart'));
       			chart.draw(data, options);
@@ -50,16 +71,37 @@ var App = new(function(){
 				var data = google.visualization.arrayToDataTable(graphData);
 				console.log('DRAWING');
 				var options = {
-			        title: 'Goverment Funding',
-			        chartArea: {width: '50%'},
-			        hAxis: {
-			          title: 'Funding',
-			          minValue: 0
-			        },
-			        vAxis: {
-			          title: 'Party'
-			        }
-			      };
+				  hAxis: {
+				    titleTextStyle: {color: '#607d8b'}, 
+				    gridlines: { count:0}, 
+				    textStyle: { color: '#b0bec5', fontName: 'Roboto', fontSize: '12', bold: true}
+				  },
+				  vAxis: {
+				    minValue: 0, 
+				    gridlines: {color:'#37474f', count:4}, 
+				    baselineColor: 'transparent'
+				  },
+				  legend: {position: 'top', alignment: 'center', textStyle: {color:'#607d8b', fontName: 'Roboto', fontSize: '12'} },
+				  colors: ["#3f51b5","#2196f3","#03a9f4","#00bcd4","#009688","#4caf50","#8bc34a","#cddc39"],
+				  areaOpacity: 0.24,
+				  lineWidth: 1,
+				  backgroundColor: 'transparent',
+				  chartArea: {
+				    backgroundColor: "transparent",
+				    width: '100%',
+				    height: '80%'
+				  },
+				      height:200, // example height, to make the demo charts equal size
+				      width:400,
+				      pieSliceBorderColor: '#263238',
+				      pieSliceTextStyle:  {color:'#607d8b' },
+				      pieHole: 0.9,
+				      bar: {groupWidth: "40" },
+				      colorAxis: {colors: ["#3f51b5","#2196f3","#03a9f4","#00bcd4"] },
+				      backgroundColor: 'transparent',
+				      datalessRegionColor: '#37474f',
+				      displayMode: 'regions'
+				    };
 
 				var chart = new google.visualization.BarChart(document.getElementById('top_funding_chart'));
       			chart.draw(data, options);
@@ -80,16 +122,37 @@ var App = new(function(){
 				var data = google.visualization.arrayToDataTable(graphData);
 				console.log('DRAWING');
 				var options = {
-			        title: 'Goverment Funding, Member fee & Donations',
-			        chartArea: {width: '50%'},
-			        hAxis: {
-			          title: '',
-			          minValue: 0
-			        },
-			        vAxis: {
-			          title: 'Party'
-			        }
-			      };
+				  hAxis: {
+				    titleTextStyle: {color: '#607d8b'}, 
+				    gridlines: { count:0}, 
+				    textStyle: { color: '#b0bec5', fontName: 'Roboto', fontSize: '12', bold: true}
+				  },
+				  vAxis: {
+				    minValue: 0, 
+				    gridlines: {color:'#37474f', count:4}, 
+				    baselineColor: 'transparent'
+				  },
+				  legend: {position: 'top', alignment: 'center', textStyle: {color:'#607d8b', fontName: 'Roboto', fontSize: '12'} },
+				  colors: ["#3f51b5","#2196f3","#03a9f4","#00bcd4","#009688","#4caf50","#8bc34a","#cddc39"],
+				  areaOpacity: 0.24,
+				  lineWidth: 1,
+				  backgroundColor: 'transparent',
+				  chartArea: {
+				    backgroundColor: "transparent",
+				    width: '100%',
+				    height: '80%'
+				  },
+				      height:200, // example height, to make the demo charts equal size
+				      width:400,
+				      pieSliceBorderColor: '#263238',
+				      pieSliceTextStyle:  {color:'#607d8b' },
+				      pieHole: 0.9,
+				      bar: {groupWidth: "40" },
+				      colorAxis: {colors: ["#3f51b5","#2196f3","#03a9f4","#00bcd4"] },
+				      backgroundColor: 'transparent',
+				      datalessRegionColor: '#37474f',
+				      displayMode: 'regions'
+				    };
 
 				var chart = new google.visualization.BarChart(document.getElementById('donation_funding_chart'));
       			chart.draw(data, options);
