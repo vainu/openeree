80017a918250df0bab1af50847763d6cc5332ed0
renereose <rene_reose@hotmail.com>
2016-02-24 13:35:39 +0200

    CORS domain

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(function (req, res, next) {
 
 //CORS middleware
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'example.com');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
